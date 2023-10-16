96cf700908a08a9c85189ee0a9237d0a57320189
Metalex84 <122928289+Metalex84@users.noreply.github.com>
2023-10-16 21:37:15 +0200

    Create Metalex84.c
    1st iteration without error handling

---
+++ Retos/Reto #40 - TABLA DE MULTIPLICAR [Fácil]/c/Metalex84.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (void)
+{
+    unsigned int value;
+    unsigned int i;
+
+    printf("TABLA DE MULTIPLICAR\nDime un numero... ");
+    scanf("%d", &value);
+    i = 1;
+    while (i <= 10 && value != 0)
+    {
+        printf("%d x %d = %d\n", value, i, i * value);
+        i++;
+    }
+    return (0);
+}
